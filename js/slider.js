a5c4540e92c504804db77baaf1e68b20191d0524
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-03-31 19:00:54 -0300

    Que verguenza, SLIDER JQUERY

--- js/slider.js
+++ js/slider.js
@@ -1,48 +1,33 @@
-window.onload = function(){
-  const left1 = document.querySelector("#izq1")
-  const right1 = document.querySelector("#der1")
-  const left2 = document.querySelector("#izq2")
-  const right2 = document.querySelector("#der2")
-  const left3 = document.querySelector("#izq3")
-  const right3 = document.querySelector("#der3")
-  const slider1 = document.querySelector("#slider-1")
-  const slider2 = document.querySelector("#slider-2")
-  const slider3 = document.querySelector("#slider-3")
-  const slider = document.querySelector("#slider")
-  
-  let ancho = window.getComputedStyle(slider1).getPropertyValue("width")
-  // left.style.left = ancho
-  // console.log(window.getComputedStyle(slider).getPropertyValue("left"))
-  // right.style.right = "-"+ancho
-  right1.addEventListener("click",()=>{
-    slider.style.marginLeft = "-100%"
-  })
-  right2.addEventListener("click",()=>{
-    slider.style.marginLeft = "-200%"
-  })
-  right3.addEventListener("click",()=>{
-    slider.style.marginLeft = "0%"
-  })
-  left1.addEventListener("click",()=>{
-    slider.style.marginLeft = "-200%"
-  })
-  left2.addEventListener("click",()=>{
-    slider.style.marginLeft = "0%"
-  })
-  left3.addEventListener("click",()=>{
-    slider.style.marginLeft = "-100%"
-  })
-  // left.addEventListener("click",()=>{
-  //   let margin = window.getComputedStyle(slider).getPropertyValue("margin-left");
-  //   let ancho = window.getComputedStyle(slider1).getPropertyValue("width")
-  //   ancho = "-"+ancho.slice(0,ancho.length-2);
-  //   margin = margin.slice(0,margin.length-2);
-  //   if(ancho == margin){
-  //     slider.style.marginLeft = "0%"
-  //   }else if(margin == 0){
-  //     slider.style.marginLeft = "-200%"
-  //   }else{
-  //     slider.style.marginLeft = "-100%"
-  //   }
-  // })
-}
+$(document).ready(function(){
+  const slider = $('.slider')
+  const btnSiguiente = $('#der')
+  const btnAnterior = $('#izq')
+
+  //Se asa el ultimo slide al primer lugar
+  $('.slider .equipamento-slider:last').insertBefore('.slider .equipamento-slider:first');
+
+  //Con margen negativo se vuelve a mostrar el pirmer slide
+  slider.css('margin-left', '-100%');
+  //Funcion para que el slider se mueva a la deracha
+  function moverDerecha(){
+    slider.animate({
+      marginLeft: '-200%'
+    }, 700, function (){
+      $('.slider .equipamento-slider:first').insertAfter('.slider .equipamento-slider:last');
+      slider.css('margin-left', '-100%');
+    })
+  }
+  btnSiguiente.on('click', moverDerecha);
+
+  //Funcion para que el slide se mueva a la izquierda
+  function moverIzquierda(){
+    $('.slider .equipamento-slider:last').insertBefore('.slider .equipamento-slider:first');
+    slider.css('margin-left', '-200%');
+    slider.animate({
+      marginLeft: '-100%'
+    }, 700)
+  }
+
+  btnAnterior.on('click', moverIzquierda);
+
+})
