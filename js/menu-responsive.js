4c2b8a1b1d7871bb8afdf8686d3be72f837e39bc
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-04-02 11:28:06 -0300

    Menu Mobile

---
+++ js/menu-responsive.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.menu-mobile')
+const btnMenu = document.querySelector('.icono-menu')
+const close = document.querySelector('.close')
+
+btnMenu.addEventListener('click',()=>{
+  menu.style.right = 0;
+})
+
+close.addEventListener('click',()=>{
+  menu.style.right = "-40%";
+})
